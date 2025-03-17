e6dfefe1938e63ad79cb9b4966475e82fee1bcd6
Wilmer Betancourt <wilmergabybet@gmail.com>
2025-03-17 13:26:09 -0500

    Creacion de paginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { PropiedadesComponent } from './pages/propiedades/propiedades.component';
+import { ReservasComponent } from './pages/reservas/reservas.component';
+import { ClientesComponent } from './pages/clientes/clientes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: 'home', component: HomeComponent},
+    {path: 'navbar', component: NavBarComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'propiedades', component: PropiedadesComponent},
+    {path: 'reservas', component: ReservasComponent},
+    {path: 'clientes', component: ClientesComponent},
+
+
+    {path: '', redirectTo:'home', pathMatch:'full'},
+
+
+];
---
+++ src/app/components/tabla1/tabla1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabla1',
+  imports: [],
+  templateUrl: './tabla1.component.html',
+  styleUrl: './tabla1.component.css'
+})
+export class Tabla1Component {
+
+}
